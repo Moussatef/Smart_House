e98a143a36a5691b005b28406a8084ae90e27739
Moussatef <otman.moussetaf@gmail.com>
2022-03-30 19:02:22 +0000

    add scss folders

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { PublicComponent } from './public.component';
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    SharedModule
+    
   ]
 })
 export class PublicModule { }
