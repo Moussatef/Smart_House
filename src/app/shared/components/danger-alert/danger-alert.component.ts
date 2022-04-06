4d4ae0515d579be654d92b2e2aa3d937aa76d8e0
Moussatef <otman.moussetaf@gmail.com>
2022-04-06 09:40:45 +0000

    form validation

---
+++ src/app/shared/components/danger-alert/danger-alert.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-danger-alert',
+  templateUrl: './danger-alert.component.html',
+  styleUrls: ['./danger-alert.component.css']
+})
+export class DangerAlertComponent implements OnInit {
+
+  @Input() description! : String;
+  @Input() type! : String;
+
+  classType : String = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.classType = this.type == "danger" ? "alert-danger" : this.type== "success" ? "alert-success" : "alert-info";
+
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DangerAlertComponent } from './components/danger-alert/danger-alert.component';
 
 
 
 @NgModule({
   declarations: [
     CardComponent,
-    NavbarComponent
+    NavbarComponent,
+    DangerAlertComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    DangerAlertComponent,
     NavbarComponent,
     CardComponent
   ],
